714d2ec814da150b101cdf6ddef291fafe80f58f
prakhar7s <prakharshrivastava971@gmail.com>
2021-06-04 21:45:44 +0530

    working on main page

---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "../styles/main.css";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="left-section">ad sd</div>
+      <div className="right-section">
+        <div className="image">
+          <img
+            src="https://images.unsplash.com/photo-1601933973783-43cf8a7d4c5f?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+            alt="pic"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
